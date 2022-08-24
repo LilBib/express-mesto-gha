4d89c57d7a46df09c376cc4c5bb27d8f96bacebe
LilBib <vovchiksuper@yandex.ru>
2022-08-25 00:48:57 +0300

    Первый коммит 14пр

--- routes/cards.js
+++ routes/cards.js
@@ -10,15 +10,43 @@ router.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required().uri(),
+      // eslint-disable-next-line no-useless-escape
+      link: Joi.string().required().pattern(/^[htps]{4,5}\:\/{2}([[w]{3}\.])?[\w\-\.\~\:\/\?\#\@\!\$\&\'\(\)\*\+\,\;\=\[\]]+/m, 'link'),
     }).unknown(true),
   }),
   auth,
   createCard,
 );
 router.get('/', auth, getCards);
-router.delete('/:cardId', auth, deleteCard);
-router.put('/:cardId/likes', auth, likeCard);
-router.delete('/:cardId/likes', auth, dislikeCard);
+router.delete(
+  '/:cardId',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().required().pattern(/\w{24}/, 'id'),
+    }).unknown(true),
+  }),
+  auth,
+  deleteCard,
+);
+router.put(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().required().pattern(/\w{24}/, 'id'),
+    }).unknown(true),
+  }),
+  auth,
+  likeCard,
+);
+router.delete(
+  '/:cardId/likes',
+  celebrate({
+    params: Joi.object().keys({
+      cardId: Joi.string().required().pattern(/\w{24}/, 'id'),
+    }).unknown(true),
+  }),
+  auth,
+  dislikeCard,
+);
 
 module.exports = router;
