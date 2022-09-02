2732ea911f582d76326e201338d878a6e9ba31b4
LilBib <vovchiksuper@yandex.ru>
2022-09-02 15:50:02 +0300

    Добавление роута краш-теста

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.options('*', cors());
 app.use(cors());
 app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post(
   '/signin',
   celebrate({
